8fa8fc38989ee34cf3f0020303eebbe9abbe9d18
Rodrigo Melo <contato.rodrigomelo@gmail.com>
2021-02-02 17:07:22 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to Rodrigo!!</a>
+          Welcome to Rodrigo!!
         </h1>
 
         <p className="description">
